88692b194dbf577574826721d99ce75b25b7b8a9
vista-treat <91408295+pritesh177@users.noreply.github.com>
2023-02-13 22:28:10 +0530

    Update Graph.tsx

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -55,8 +55,8 @@ class Graph extends Component<IProps, {}> {
 
   componentDidUpdate() {
     if (this.table) {
-      this.table.update(
-        DataManipulator.generateRow(this.props.data),
+      this.table.update([
+        DataManipulator.generateRow(this.props.data),] as unknown as TableData
       );
     }
   }
